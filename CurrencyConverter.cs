6ba07ff972e80e7bde1d0e2c50c3d4dc23374a9e
Andeng Mercado <158003221+andengsome@users.noreply.github.com>
2025-02-05 17:54:30 +0800

    Create CurrencyConverter.cs

---
+++ CurrencyConverter.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class CalculateInterest
+{
+    public static void Main(string[] args)
+    {
+        Console.Write ("Enter amount in USD: ");
+        double amount = double.Parse(Console.ReadLine());
+        Console.Write ("Enter exchange rate from USD to EUR: ");
+        double rate = double.Parse(Console.ReadLine());
+        
+        double convertedAmount = amount * rate;
+        
+        Console.WriteLine ("Amount in EUR: {0:F2}", convertedAmount);
+        
+    }
+}
